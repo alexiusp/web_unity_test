7bf94b5890c804afcc5e40ace4d85b8112d3566a
Aleksei Podgaev <podgaev@gmail.com>
2018-09-25 18:04:33 +0200

    * Logger functionality moved to service * console component refactored - prepared to separate from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import Logger from './Logger';
 
 const BASE_PATH = (process.env.REACT_APP_UNITY_PATH || 'https://nncms.s3-eu-central-1.amazonaws.com/assets/edison/exercises/brain');
 const BUILD_PATH = BASE_PATH + '/Build';
@@ -24,7 +25,7 @@ interface IAppState {
   config: string;
   configPath: string;
   console: string;
-  debug: string[];
+  counter: number;
   options: string;
   progress: number;
   settings: string;
@@ -52,7 +53,7 @@ class App extends React.Component<{}, IAppState> {
     }),
     configPath: `${BUILD_PATH}/Production.json`,
     console: '>',
-    debug: [],
+    counter: 0,
     options: '{ "difficulty": 0, "mode": "training" }',
     progress: 0,
     settings: JSON.stringify({
@@ -86,7 +87,13 @@ class App extends React.Component<{}, IAppState> {
     script.onload = this.onUnityInitialize;
     script.async = true;
     document.body.appendChild(script);
-    this.log('Test started!');
+    Logger.log('Test started!');
+  }
+
+  public stopTest = () => {
+    this.stopTimers();
+    this.setState({ start: false });
+    Logger.log('Stop!');
   }
 
   public onUnityProgress = (instance: IUnityInstance, progress: number) => {
@@ -94,18 +101,18 @@ class App extends React.Component<{}, IAppState> {
   }
 
   public onUnityInitialize = () => {
-    this.log('UnityLoader onLoad called');
+    Logger.log('UnityLoader onLoad called');
     // unity loader script loaded - ready to load engine
     const path = this.state.configPath + '?rnd=' + getCacheBuster();
     this.instance = UnityLoader.instantiate(CANVAS_ID, path, { onProgress: this.onUnityProgress });
-    this.initTimer = setInterval(this.progress, 500);
+    this.initTimer = setInterval(this.onTimer, 500);
   }
 
   // 'app ready' handler called when core app loaded
   public appReady = () => {
-    this.log('appReady called');
-    clearInterval(this.initTimer);
-    this.startTimer = setInterval(this.progress, 500);
+    this.stopTimers();
+    Logger.log('appReady called');
+    this.startTimer = setInterval(this.onTimer, 500);
     if (this.state.auto) {
       this.loadApp();
     }
@@ -113,9 +120,8 @@ class App extends React.Component<{}, IAppState> {
 
   // 'engine ready' handler called when engine finished loading
   public engineReady = () => {
-    this.log('engineReady called');
-    clearInterval(this.startTimer);
-    clearInterval(this.initTimer);
+    this.stopTimers();
+    Logger.log('engineReady called');
     if (this.state.auto) {
       this.loadExercise();
     }
@@ -123,9 +129,8 @@ class App extends React.Component<{}, IAppState> {
 
   // 'assets loaded' handler called when exercise assets were loaded
   public exerciseReady = () => {
-    this.log('exerciseReady called');
-    clearInterval(this.startTimer);
-    clearInterval(this.initTimer);
+    this.stopTimers();
+    Logger.log('exerciseReady called');
     if (this.state.auto) {
       this.startExercise();
     }
@@ -144,11 +149,12 @@ class App extends React.Component<{}, IAppState> {
   }
 
   public completeExercise = (result: any) => {
-    this.log(`completeExercise: ${JSON.stringify(result)}`);
+    Logger.log(`completeExercise: ${JSON.stringify(result)}`);
   }
 
   public onError = (e: Error) => {
-    this.log(`Error: ${e.message}`);
+    this.stopTimers();
+    Logger.log(`Error: ${e.message}`);
   }
 
   public componentDidCatch(e: Error) {
@@ -156,8 +162,7 @@ class App extends React.Component<{}, IAppState> {
   }
 
   public render() {
-    const debug = this.state.debug.join('\n');
-    const consoleContent = (debug ? debug + '\n' : '') + this.state.console;
+    const debug = Logger.print();
     return (
       <div className="App">
         <header className="App-header">
@@ -165,19 +170,20 @@ class App extends React.Component<{}, IAppState> {
         </header>
         <div className="controls">
           <div className="controls-row">
-            <label>auto execute: <input type="checkbox" checked={this.state.auto} onChange={this.handleCheckboxChange('auto')} /></label>&nbsp;
+            <label>auto execute: <input name="auto" type="checkbox" checked={this.state.auto} onChange={this.handleCheckboxChange('auto')} /></label>&nbsp;
             <button onClick={this.startTest}>initialize</button>
+            <button onClick={this.stopTest}>stop</button>
           </div>
           <div className="controls-row">
-            <label>app config: <input value={this.state.config} onChange={this.handleInputChange('config')} /></label>&nbsp;
+            <label>app config: <input name="config" value={this.state.config} onChange={this.handleInputChange('config')} /></label>&nbsp;
             <button onClick={this.loadApp}>load app</button>
           </div>
           <div className="controls-row">
-            <label>settings: <input value={this.state.settings} onChange={this.handleInputChange('settings')} /></label>&nbsp;
+            <label>settings: <input name="settings" value={this.state.settings} onChange={this.handleInputChange('settings')} /></label>&nbsp;
             <button onClick={this.loadExercise}>load exercise</button>
           </div>
           <div className="controls-row">
-            <label>options: <input value={this.state.options} onChange={this.handleInputChange('options')} /></label>&nbsp;
+            <label>options: <input name="options" value={this.state.options} onChange={this.handleInputChange('options')} /></label>&nbsp;
             <button onClick={this.startExercise}>start exercise</button>
           </div>
         </div>
@@ -188,7 +194,8 @@ class App extends React.Component<{}, IAppState> {
           <canvas id={CANVAS_ID} />
         </div>
         <div className="debug">
-          <textarea className="debug-console" rows={20} cols={80} onKeyPress={this.handleConsoleKey} onChange={this.handleConsoleChange} value={consoleContent} />
+          <pre className="debug-console">{debug}</pre>
+          <input id="debug-input" className="debug-input" type="text" value={this.state.console} onKeyPress={this.handleConsoleKey} onChange={this.handleConsoleChange} />
         </div>
       </div>
     );
@@ -206,38 +213,32 @@ class App extends React.Component<{}, IAppState> {
     this.setState(statePart);
   }
 
-  private handleConsoleChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    e.stopPropagation();
-    e.preventDefault();
+  private handleConsoleChange: React.ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // e.stopPropagation();
+    // e.preventDefault();
     const lines = e.target.value.split('\n');
     this.setState({ console: lines[lines.length - 1] });
   }
 
-  private handleConsoleKey: React.KeyboardEventHandler<HTMLTextAreaElement> = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  private handleConsoleKey: React.KeyboardEventHandler<HTMLInputElement> = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.charCode !== 13) {
       return;
     }
     const cmd = (`${this.state.console}`).slice(1);
     this.setState({ console: '>' });
-    this.log(`Unknown command: ${cmd}`);
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
-  private log = (msg: string) => {
-    this.setState({
-      debug: [...this.state.debug, msg],
-    });
+    Logger.log(`Unknown command: ${cmd}`);
+    // e.stopPropagation();
+    // e.preventDefault();
   }
 
   // sending messages to game instance
   private sendMessage: (objectName: string, methodName: string, value: any) => void = (objectName, methodName, value) => {
     if (this.instance) {
       const params = typeof value === 'string' ? value : JSON.stringify(value);
-      this.log(`calling SendMessage('${objectName}', '${methodName}', '${params}')`);
+      Logger.log(`calling SendMessage('${objectName}', '${methodName}', '${params}')`);
       this.instance.SendMessage(objectName, methodName, params);
     } else {
-      this.log('Error: Trying to send message to not initialized instance');
+      Logger.log('Error: Trying to send message to not initialized instance');
     }
   }
 
@@ -246,14 +247,18 @@ class App extends React.Component<{}, IAppState> {
   private exerciseInit = (settings: any) => this.sendMessage('Main', 'InitializeExercise', settings);
   private exerciseStart = (options: any) => this.sendMessage('Main', 'StartExercise', options);
 
-  private progress = () => {
-    const messages = [...this.state.debug];
-    const msg = messages.pop();
-    messages.push(msg + '.');
-    this.setState({
-      debug: [...messages],
-    });
+  private onTimer = () => {
+    this.setState({ counter: this.state.counter + 1 });
+    Logger.progress();
   }
+
+  private stopTimers = () => {
+    this.setState({ counter: 0 });
+    clearInterval(this.startTimer);
+    clearInterval(this.initTimer);
+    Logger.stop();
+  }
+
 }
 
 export default App;
