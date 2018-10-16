5c424aa93e5746e98ab4b22e0cb022bfdedd81b0
Aleksei Podgaev <podgaev@gmail.com>
2018-10-16 15:41:00 +0200

    * size correction

--- src/unity/UnityView.tsx
+++ src/unity/UnityView.tsx
@@ -4,6 +4,6 @@ import { CANVAS_ID } from '../constants';
 
 export default function UnityView() {
   return (
-    <div id={CANVAS_ID} style={{ width: '960px', height: '600px' }} />
+    <div id={CANVAS_ID} style={{ width: '970px', height: '600px' }} />
   );
 }
